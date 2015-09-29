09a625c17f1b145ec204d220cc2f5d5ab663c639
Konstantin <khmel7@gmail.com>
2015-09-29 12:11:40 +0300

    28-08 Lesya

--- app/src/main/java/com/xmel/photogallery/PhotoGalleryFragment.java
+++ app/src/main/java/com/xmel/photogallery/PhotoGalleryFragment.java
@@ -1,9 +1,51 @@
 package com.xmel.photogallery;
 
 import android.app.Fragment;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.GridView;
+
+import java.io.IOException;
 
 /**
  * Created by Admin on 28.09.2015.
  */
 public class PhotoGalleryFragment extends Fragment {
+    private static final String TAG = "PhotoGalleryFragment";
+    GridView mGridView;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRetainInstance(true);
+        new FetchItemTask().execute();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_photo_gallery, container, false);
+        mGridView = (GridView) v.findViewById(R.id.gridView);
+        return v;
+    }
+
+    private class FetchItemTask extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                String result = new FlickrFetchr().getUrl("http://www.google.com");
+                Log.i(TAG, "Fetched contents of URL: " + result);
+            } catch (IOException e) {
+                Log.e(TAG, "Faied to fetch URL:" + e);
+            }
+
+            return null;
+        }
+    }
 }
